03e77603e49d9716ed1a55593cfe3686b3f29133
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-08-27 13:23:30 -0400

    Created Walks Model/repo/interface and implemented WalkerViewModel displaying walker, recent walk information, and total walk time acquired.

---
+++ DogGo/Repositories/IWalksRepository.cs
@@ -0,0 +1,11 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IWalksRepository
+    {
+        List<Walks> GetWalksByWalkerId(int walkerId);
+    }
+}
---
+++ DogGo/Repositories/WalksRepository.cs
@@ -0,0 +1,78 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DogGo.Repositories
+{
+    public class WalksRepository : IWalksRepository
+    {
+        private readonly IConfiguration _config;
+
+        public WalksRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Walks> GetWalksByWalkerId(int walkerId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                            SELECT wk.Id, wk.[Date], wk.Duration, wk.WalkerId, wk.DogId, d.Id, d.[Name] AS 'DogName', d.Breed, d.OwnerId
+                                            FROM Walks wk
+                                            LEFT JOIN Dog d
+                                            ON wk.DogId = d.Id
+                                            WHERE wk.WalkerId = @walkerId
+                                            ORDER BY 'DogName'
+                                       ";
+
+                    cmd.Parameters.AddWithValue("@walkerId", walkerId);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Walks> walks = new List<Walks>();
+
+                    while (reader.Read())
+                    {
+                        Walks walk = new Walks()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                            Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
+                            WalkerId = walkerId,
+                            DogId = reader.GetInt32(reader.GetOrdinal("DogId")),
+                            Dog = new Dog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("DogId")),
+                                Name = reader.GetString(reader.GetOrdinal("DogName")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId"))
+                            }
+                        };
+
+                        walks.Add(walk);
+                    }
+                    reader.Close();
+                    return walks;
+
+
+                }
+            }
+        }
+
+
+    }
+}
